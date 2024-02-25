830140602db71004c1db71a651d8e2a5382faf4b
tobacoker <tobicco93@gmail.com>
2024-02-24 23:36:53 -0500

    functions being added ti java sacript assignment

--- Main/assets/js/logic.js
+++ Main/assets/js/logic.js
@@ -37,55 +37,78 @@ function startQuiz() {
 }
 
 function getQuestion() {
+
   // get current question object from array
 let currentQuestion = questions[currentQuestionIndex];
+
   // update title with current question
 document.getElementById('question-title').textContent = currentQuestion.title;
+
   // clear out any old question choices
+  choices.innerHTML = '';
 
   // loop over the choices for each question
+  for (let i =0; i <currentQuestion.choices.length; i++)
   // get the number of questions
+
   let numberOfQuestions; // assign it the value of the length of the questions array
   for (let i = 0; i < numberOfQuestions; i++) {
 
     // create a new button for each choice, setting the label and value for the button
+let choiceButton = document.createElement('button');
+choiceButton.textContent = currentQuestion.choices[i];
+choiceButton.setAttribute('value',currentQuestion.choices[i]);
 
     // display the choice button on the page
-
+choices.appendChild(choiceButton);
   }
 }
 
 function questionClick(event) {
   // identify the targeted button that was clicked on
+let clickedButton = event.target;
 
   // if the clicked element is not a choice button, do nothing.
-
+if (!clickedButton.matches('button')){
+  return;
+}
 
   // check if user guessed wrong
-  // if () {
+  if (clickedButton.value !== questions[currentQuestionIndex].answer) {
+
   // if they got the answer wrong, penalize time by subtracting 15 seconds from the timer
+time -= 15;
+
   // recall the timer is the score they get at the end
 
   // if they run out of time (i.e., time is less than zero) set time to zero so we can end quiz
-
+if (time < 0) {
+  time = 0;
+}
 
   // display new time on page
+  timerEl.textContent = time;
 
   // play "wrong" sound effect
+  sfxWrong.play();
 
   // display "wrong" feedback on page
-
+feedbackEl.textContent = 'wrong;'
 } else {
   // play "right" sound effect
+  sfxRight.play()
 
   // display "right" feedback on page by displaying the text "Correct!" in the feedback element
-
+feedbackEl.textContent = 'correct';
 }
 // flash right/wrong feedback on page for half a second
 // set the feedback element to have the class of "feedback"
+feedbackEl.classList.add('feedback');
 
 
 // after one second, remove the "feedback" class from the feedback element
+setTimeout(function() {
+  feedbackEl.classList.remove('feedback');
 
 // move to next question
 currentQuestionIndex++;
@@ -94,27 +117,42 @@ currentQuestionIndex++;
 // if the time is less than zero and we have reached the end of the questions array,
 // call a function that ends the quiz (quizEnd function)
 // or else get the next question
+if (currentQuestionIndex === questions.length || time <=0) {
+  quizEnd();
+} else {
+  getQuestion();
+}
+},1000);
+}
 
 
 // define the steps of the QuizEnd function...when the quiz ends...
 function quizEnd() {
   // stop the timer
+  clearInterval(timerId);
 
   // show end screen
+  document.getElementById('end-screen').style.display = 'block;'
 
   // show final score
+  document.getElementById('final-score').textContent = time;
 
   // hide the "questions" section
+  questionsEl.style.display = 'none'
 }
 
 // add the code in this function to update the time, it should be called every second
 function clockTick() {
   // right here - update time
+  time--;
 
   // update the element to display the new time value
+  timerEl.textContent = time;
 
   // check if user ran out of time; if so, call the quizEnd() function
-
+if (time <= 0) {
+  quizEnd();
+}
 }
 
 // complete the steps to save the high score
